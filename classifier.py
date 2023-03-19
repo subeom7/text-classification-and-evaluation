348b008bb086a2db43875a752b670e4501ebdf3c
Subeom7 <subeomkwon@gmail.com>
2023-03-19 14:44:25 -0400

    Separate classifier training and prediction

--- classifier.py
+++ classifier.py
@@ -1,28 +1,15 @@
+import joblib
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from sklearn.pipeline import Pipeline
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.datasets import fetch_20newsgroups
 
 app = Flask(__name__)
 
 # allows requests from different origins (e.g. different domains or ports) to access resources on the server
 CORS(app)
-# Load dataset and train the model
-categories = ['soc.religion.christian','comp.graphics', 'sci.med', 
-              'sci.electronics','sci.space', 'sci.electronics', 
-              'sci.crypt','rec.sport.baseball', 'rec.sport.hockey', 
-              'rec.autos','talk.politics.guns']
 
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-
-text_clf = Pipeline([
-    ('tfidf', TfidfVectorizer()),
-    ('clf', MultinomialNB())
-])
-
-text_clf.fit(twenty_train.data, twenty_train.target)
+# Load the saved model and the twenty_train object
+text_clf = joblib.load('text_classifier.joblib')
+twenty_train = joblib.load('twenty_train.joblib')
 
 category_map = {
     'soc.religion.christian': 'Religion/Christian',
@@ -46,7 +33,7 @@ def classify_text():
 
         # Perform the classification
         predicted = text_clf.predict(user_inputs)
-        for doc, category in zip(user_inputs, predicted):
+        for category in predicted:
             output_string = category_map[twenty_train.target_names[category]]
             return jsonify(result=output_string)
 
