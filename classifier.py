fbebb87cf599d2ff07b47ff6f1151b22dfc54f26
Subeom7 <subeomkwon@gmail.com>
2023-03-24 17:11:54 -0400

    Add feature to display important words

--- classifier.py
+++ classifier.py
@@ -1,4 +1,6 @@
+#FLASK API Version
 import joblib
+import re
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -24,6 +26,36 @@ category_map = {
     'talk.politics.guns': 'Politics/Guns',
 }
 
+def find_important_words(sentence, text_clf, original_class, top_n=5):
+    words = sentence.split()
+    original_proba = text_clf.predict_proba([sentence])[0][original_class]
+
+    word_diffs = []
+    processed_words = set()
+    for i, word in enumerate(words):
+        # Skip the word if it has already been processed
+        if word.lower() in processed_words:
+            continue
+
+        # Remove the current word from the original sentence
+        without_word = ' '.join(w for w in words if w.lower() != word.lower())
+        new_proba = text_clf.predict_proba([without_word])[0][original_class]
+        diff = abs(original_proba - new_proba)
+        word_diffs.append((word, diff))
+
+        # Add the word to the set of processed words
+        processed_words.add(word.lower())
+
+    # Sort words by their importance (difference in probabilities)
+    sorted_words = sorted(word_diffs, key=lambda x: x[1], reverse=True)
+
+    # Calculate the total importance (sum of diffs)
+    total_importance = sum(diff for _, diff in sorted_words)
+
+    # Return the top N important words and their importance percentage
+    return [(re.sub(r'\W', '', word).capitalize()) + "(" + f"{round(diff / total_importance * 100)}%" + ")" for word, diff in sorted_words[:top_n]]
+
+
 @app.route('/classify', methods=['POST'])
 def classify_text():
     if request.method == 'POST':
@@ -31,11 +63,16 @@ def classify_text():
         input_text = request.json['text']
         user_inputs = [input_text]
 
-        # Perform the classification
-        predicted = text_clf.predict(user_inputs)
-        for category in predicted:
-            output_string = category_map[twenty_train.target_names[category]]
-            return jsonify(result=output_string)
+        # Calculate top_n based on the length of the input text
+        num_words = len(input_text.split())
+        topN = max(3, int(num_words * 0.1))  # Set top_n to 10% of the number of words, with a minimum of 1
+
+         # Perform the classification
+        predicted = text_clf.predict([input_text])
+        predicted_class = int(predicted[0])  # Convert the predicted class to an integer
+        important_words = find_important_words(input_text, text_clf, predicted_class, top_n=topN)
+        output_string = category_map[twenty_train.target_names[predicted_class]]
+        return jsonify(result=output_string, words='\n'.join(important_words))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
