ab8306bacf90fd579508371207c875d995bde5b3
Subeom7 <subeomkwon@gmail.com>
2023-04-19 14:04:02 -0400

    Adjust box overflow and size of AI result and word

--- client/src/components/OutputDisplay.js
+++ client/src/components/OutputDisplay.js
@@ -5,11 +5,11 @@ const OutputDisplay = ({ responseData }) => {
     <>
       <div style={{ fontSize: '24px', padding: '10px', marginBottom: '-20px', fontWeight: 'bold' }}>AI Classification: </div>
       {responseData && (
-        <p style={{ fontSize: '24px', padding: '20px', backgroundColor: '#F5F5F5', borderRadius: '10px', boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.25)', overflowWrap: 'break-word', maxHeight: '300px', overflowY: 'auto' }}>{`${responseData.result}`}</p>
+        <p style={{ fontSize: '24px', padding: '20px', backgroundColor: '#F5F5F5', borderRadius: '10px', boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.25)', overflowWrap: 'break-word', maxHeight: '300px'}}>{`${responseData.result}`}</p>
       )}
       <div style={{ fontSize: '24px', padding: '10px', marginBottom: '-20px', fontWeight: 'bold' }}>Important Words:</div>
       {responseData && (
-        <p style={{ fontSize: '24px', padding: '20px', backgroundColor: '#F5F5F5', borderRadius: '10px', boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.25)', overflowWrap: 'break-word', whiteSpace: 'pre-wrap', maxHeight: '300px', overflowY: 'auto' }}>{responseData.words}</p>
+        <p style={{ fontSize: '24px', padding: '20px', backgroundColor: '#F5F5F5', borderRadius: '10px', boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.25)', overflowWrap: 'break-word', whiteSpace: 'pre-wrap', maxHeight: '200px', overflowY: 'auto' }}>{responseData.words}</p>
       )}
     </>
   );
