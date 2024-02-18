2e6b5cd3e3b213d88b55f1da0bc16822dcf34f42
Subeom Kwon <subeomkwon@gmail.com>
2024-02-18 21:26:13 +0900

    Add JWT token verification logic

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask import Flask, request, jsonify, render_template, send_from_directory, abort
 from flask_cors import CORS
 from flask.json import JSONEncoder
 from bson import ObjectId
-from classifier import text_clf, classify, save_database
-from database import get_classification_history
-from pymongo import MongoClient
-from dotenv import load_dotenv
-from database import update_classification_history
+import jwt
 import os
 import certifi
+from classifier import text_clf, classify, save_database
+from database import get_classification_history, update_classification_history
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
+# 환경변수 로드
 load_dotenv()
 
 MONGODB_URI = os.environ["MONGODB_URI"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
 db = client.User_History
@@ -27,6 +29,22 @@ app = Flask(__name__, template_folder="client/build", static_folder="client/buil
 app.json_encoder = CustomJSONEncoder
 CORS(app)
 
+# Decorator that verifies JWT Token
+def token_required(f):
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            # remove "Bearer " part from the Bearer Token
+            if token.startswith('Bearer '):
+                token = token[7:]  # Only extract string following "Bearer "
+            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except Exception as e:
+            return jsonify({'message': 'Token is invalid or expired!', 'error': str(e)}), 403
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
@@ -36,6 +54,7 @@ def serve(path):
         return send_from_directory(app.template_folder, "index.html")
 
 @app.route('/classify', methods=['POST'])
+@token_required
 def classify_text():
     if request.method == 'POST':
         input_text = request.json['text']
@@ -46,7 +65,8 @@ def classify_text():
 
         return jsonify(result=output_string, words='\n'.join(important_words), document_id=document_id)
     
-@app.route('/database', methods=['POST'])
+@app.route('/database', methods=['POST'], endpoint='database_post')
+@token_required
 def save_to_database():
     if request.method == 'POST':
         input_text = request.json['text']
